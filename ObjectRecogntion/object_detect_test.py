fd365611fea4a94edcda10a435e5af4e1694516b
Tyler Aratani <tyar7148@colorado.edu>
2018-02-27 18:55:41 +0000

    upadte code to classify images

--- ObjectRecogntion/object_detect_test.py
+++ ObjectRecogntion/object_detect_test.py
@@ -1,12 +1,16 @@
 import numpy as np 
 import os
 import sys
+import time
 import tensorflow as tf
 
 from collections import defaultdict
 from io import StringIO
-from time import sleep
 from picamera import PiCamera
+
+sys.path.append("..")
+
+from utils import label_map_util
 
 import cv2
 
@@ -14,7 +18,7 @@ import cv2
 camera = PiCamera()
 
 # path to frozen detection graph
-PATH_TO_CKPT = 'ssd_mobilenet_v1_coco_11_06_2017/frozen_interface_model'
+PATH_TO_CKPT = 'ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
@@ -22,6 +26,7 @@ PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
 
 # load frozen model into memory
+t0 = time.time()
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
@@ -29,23 +34,34 @@ with detection_graph.as_default():
     serialized_graph = fid.read()
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
+t1 = time.time()
+print('Frozen model loaded: ' + str(t1-t0) + ' s')
 
 def detectObstacle(image_np):
-	# actual detection
-	(boxes, scores, classes, num) = sess.run(
-		[detection_boxes, detection_scores, detection_classes, num_detections],
-		feed_dict={image_tensor: image_np_expanded})
-	objects = []
+    # actual detection
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    (boxes, scores, classes, num) = sess.run(
+	[detection_boxes, detection_scores, detection_classes, num_detections],
+	feed_dict={image_tensor: image_np_expanded})
     threshold = 0.5
     #objects = [category_index.get(value) for index,value in enumerate(classes[0]) if scores[0,index] > 0.5]
     #print(objects['name'])
+    object_dict = {}
     for index, value in enumerate(classes[0]):
-        object_dict = {}
         if scores[0, index] > threshold:
-          	object_dict[(category_index.get(value)).get('name').encode('utf8')] = \
-                scores[0, index]
-          	objects.append(object_dict.copy())
-     print(objects)
+		midX = (boxes[0][index][1] + boxes[0][index][3])/2
+		midY = (boxes[0][index][0] + boxes[0][index][2])/2
+
+		location = 0
+		if(midX < 0.33):
+		    location = 1
+		elif(midX >= 0.33 and midX <= 0.66):
+		    location = 2
+		else:
+		    location = 3
+		object_dict[(category_index.get(value)).get('name').encode('utf8')] = location
+
+    return object_dict
 
 
 # loading label maps
@@ -55,10 +71,10 @@ category_index = label_map_util.create_category_index(categories)
 
 with detection_graph.as_default():
 	with tf.Session(graph=detection_graph) as sess:
-		# definite input and output tensors
-		image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-		detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-		detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+	    # definite input and output tensors
+	    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+	    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+	    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 	    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 	    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 	    image_np = cv2.imread('image.jpg')
@@ -67,14 +83,27 @@ with detection_graph.as_default():
 
 	    while True:
 	    	option = raw_input()
-	    	if(option=='')
-		    	# capture image
-		    	camera.start_preview()
-		    	sleep(2)
-		    	camera.capture('image.jpg')
-		    	camera.stop_preview()
-		    	image_np = cv2.imread('image.jpg')
+	    	if(option==''):
+		    # capture image
+		    os.remove('image.jpg')
+		    camera.start_preview()
+		    time.sleep(2)
+		    t0 = time.time()
+	 	    camera.capture('image.jpg')
+		    t1 = time.time()
+	 	    camera.stop_preview()
+		    print('Image taken and saved ' + str(t1-t0) + ' s')
+		    detect = False
+		    while(not detect):
+		        if(raw_input()==''):
+			    detect = True
+		    image_np = cv2.imread('image.jpg')
 
-		    	detectObstacle(image_np)
-		    if(option=='q'):
-		    	break
+		    t0 = time.time()
+		    objects = detectObstacle(image_np)
+		    t1 = time.time()
+		    if objects:
+		        print('Object detected: ' + str(t1-t0) + ' s')
+		    print(objects)
+		if(option=='q'):
+		    break
