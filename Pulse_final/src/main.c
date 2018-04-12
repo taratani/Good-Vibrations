2717bd3d3c67352e22785272e0df7ec67db14e2c
BryGo11 <bronig11@gmail.com>
2018-04-11 21:00:26 -0600

    Final project

---
+++ Pulse_final/src/main.c
@@ -0,0 +1,403 @@
+#include <stm32f4xx.h>
+#include <misc.h>			 // I recommend you have a look at these in the ST firmware folder
+#include <stm32f4xx_usart.h> // under Libraries/STM32F4xx_StdPeriph_Driver/inc and src
+#include <stm32f4xx_i2c.h>
+#include <stm32f4xx_tim.h>
+#include <stm32f4xx_rcc.h>
+
+#define MAX_STRLEN 20 // this is the maximum string length of our string in characters
+#define BUTTON_LIMIT 2500000
+volatile char received_string[MAX_STRLEN+1]; // this will hold the recieved string
+#define SLAVE_ADDRESS 0x29 // the slave address (example)
+uint8_t state = 1;
+uint8_t received_data[2];
+
+void Delay(__IO uint32_t nCount) {
+  while(nCount--) {
+  }
+}
+
+/* This funcion initializes the USART1 peripheral
+ *
+ * Arguments: baudrate --> the baudrate at which the USART is
+ * 						   supposed to operate
+ */
+void init_USART1(uint32_t baudrate){
+
+	/* This is a concept that has to do with the libraries provided by ST
+	 * to make development easier the have made up something similar to
+	 * classes, called TypeDefs, which actually just define the common
+	 * parameters that every peripheral needs to work correctly
+	 *
+	 * They make our life easier because we don't have to mess around with
+	 * the low level stuff of setting bits in the correct registers
+	 */
+	GPIO_InitTypeDef GPIO_InitStruct; // this is for the GPIO pins used as TX and RX
+	USART_InitTypeDef USART_InitStruct; // this is for the USART1 initilization
+	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
+
+	/* enable APB2 peripheral clock for USART1
+	 * note that only USART1 and USART6 are connected to APB2
+	 * the other USARTs are connected to APB1
+	 */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	/* enable the peripheral clock for the pins used by
+	 * USART1, PB6 for TX and PB7 for RX
+	 */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+	/* This sequence sets up the TX and RX pins
+	 * so they work correctly with the USART1 peripheral
+	 */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10; // Pins 6 (TX) and 7 (RX) are used
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF; 			// the pins are configured as alternate function so the USART peripheral has access to them
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// this defines the IO speed and has nothing to do with the baudrate!
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;			// this defines the output type as push pull mode (as opposed to open drain)
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// this activates the pullup resistors on the IO pins
+	GPIO_Init(GPIOA, &GPIO_InitStruct);					// now all the values are passed to the GPIO_Init() function which sets the GPIO registers
+
+	/* The RX and TX pins are now connected to their AF
+	 * so that the USART1 can take over control of the
+	 * pins
+	 */
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1); //
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
+
+	/* Now the USART_InitStruct is used to define the
+	 * properties of USART1
+	 */
+	USART_InitStruct.USART_BaudRate = baudrate;				// the baudrate is set to the value we passed into this init function
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;// we want the data frame size to be 8 bits (standard)
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;		// we want 1 stop bit (standard)
+	USART_InitStruct.USART_Parity = USART_Parity_No;		// we don't want a parity bit (standard)
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // we don't want flow control (standard)
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx; // we want to enable the transmitter and the receiver
+	USART_Init(USART1, &USART_InitStruct);					// again all the properties are passed to the USART_Init function which takes care of all the bit setting
+
+
+	/* Here the USART1 receive interrupt is enabled
+	 * and the interrupt controller is configured
+	 * to jump to the USART1_IRQHandler() function
+	 * if the USART1 receive interrupt occurs
+	 */
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // enable the USART1 receive interrupt
+
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;		 // we want to configure the USART1 interrupts
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;// this sets the priority group of the USART1 interrupts
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;		 // this sets the subpriority inside the group
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			 // the USART1 interrupts are globally enabled
+	NVIC_Init(&NVIC_InitStructure);							 // the properties are passed to the NVIC_Init function which takes care of the low level stuff
+
+	// finally this enables the complete USART1 peripheral
+	USART_Cmd(USART1, ENABLE);
+}
+
+void Configure_PC4(void) {
+    /* Set variables used */
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+
+    /* Enable clock for GPIOD */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+    /* Enable clock for SYSCFG */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+    /* Set pin as input */
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /* Tell system that you will use PD0 for EXTI_Line0 */
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOC, EXTI_PinSource4);
+
+    /* PD0 is connected to EXTI_Line0 */
+    EXTI_InitStruct.EXTI_Line = EXTI_Line4;
+    /* Enable interrupt */
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    /* Interrupt mode */
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* Triggers on rising and falling edge */
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+    /* Add to EXTI */
+    EXTI_Init(&EXTI_InitStruct);
+
+    /* Add IRQ vector to NVIC */
+    /* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI4_IRQn;
+    /* Set priority */
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+    /* Set sub priority */
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+    /* Enable interrupt */
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    /* Add to NVIC */
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+/* This function is used to transmit a string of characters via
+ * the USART specified in USARTx.
+ *
+ * It takes two arguments: USARTx --> can be any of the USARTs e.g. USART1, USART2 etc.
+ * 						   (volatile) char *s is the string you want to send
+ *
+ * Note: The string has to be passed to the function as a pointer because
+ * 		 the compiler doesn't know the 'string' data type. In standard
+ * 		 C a string is just an array of characters
+ *
+ * Note 2: At the moment it takes a volatile char because the received_string variable
+ * 		   declared as volatile char --> otherwise the compiler will spit out warnings
+ * */
+void USART_puts(USART_TypeDef* USARTx, volatile char *s){
+
+	while(*s){
+		// wait until data register is empty
+		while( !(USARTx->SR & 0x00000040) );
+		USART_SendData(USARTx, *s);
+		*s++;
+	}
+}
+
+void TIM_INT_Init()
+{
+    // Enable clock for TIM2
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    // TIM2 initialization overflow every 500ms
+    // TIM2 by default has clock of 84MHz
+    // Here, we must set value of prescaler and period,
+    // so update event is 0.5Hz or 500ms
+    // Update Event (Hz) = timer_clock / ((TIM_Prescaler + 1) *
+    // (TIM_Period + 1))
+    // Update Event (Hz) = 84MHz / ((4199 + 1) * (9999 + 1)) = 0.5 Hz
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 4199;
+    TIM_TimeBaseInitStruct.TIM_Period = 9999;
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+
+    // TIM2 initialize
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
+    // Enable TIM2 interrupt
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    // Start TIM2
+    TIM_Cmd(TIM2, ENABLE);
+
+    // Nested vectored interrupt settings
+    // TIM2 interrupt is most important (PreemptionPriority and
+    // SubPriority = 0)
+    NVIC_InitTypeDef NVIC_InitStruct;
+    NVIC_InitStruct.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+void TM_LEDS_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	/* Clock for GPIOD */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+	/* Alternating functions for pins */
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource13, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource14, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource15, GPIO_AF_TIM4);
+
+	/* Set pins */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void TM_TIMER_Init(void) {
+	TIM_TimeBaseInitTypeDef TIM_BaseStruct;
+
+	/* Enable clock for TIM4 */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+/*
+	TIM4 is connected to APB1 bus, which has on F407 device 42MHz clock
+	But, timer has internal PLL, which double this frequency for timer, up to 84MHz
+	Remember: Not each timer is connected to APB1, there are also timers connected
+	on APB2, which works at 84MHz by default, and internal PLL increase
+	this to up to 168MHz
+
+	Set timer prescaller
+	Timer count frequency is set with
+
+	timer_tick_frequency = Timer_default_frequency / (prescaller_set + 1)
+
+	In our case, we want a max frequency for timer, so we set prescaller to 0
+	And our timer will have tick frequency
+
+	timer_tick_frequency = 84000000 / (0 + 1) = 84000000
+*/
+	TIM_BaseStruct.TIM_Prescaler = 0;
+	/* Count up */
+    TIM_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+/*
+	Set timer period when it have reset
+	First you have to know max value for timer
+	In our case it is 16bit = 65535
+	To get your frequency for PWM, equation is simple
+
+	PWM_frequency = timer_tick_frequency / (TIM_Period + 1)
+
+	If you know your PWM frequency you want to have timer period set correct
+
+	TIM_Period = timer_tick_frequency / PWM_frequency - 1
+
+	In our case, for 10Khz PWM_frequency, set Period to
+
+	TIM_Period = 84000000 / 10000 - 1 = 8399
+
+	If you get TIM_Period larger than max timer value (in our case 65535),
+	you have to choose larger prescaler and slow down timer tick frequency
+*/
+    TIM_BaseStruct.TIM_Period = 8399; /* 10kHz PWM */
+    TIM_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_BaseStruct.TIM_RepetitionCounter = 0;
+	/* Initialize TIM4 */
+    TIM_TimeBaseInit(TIM1, &TIM_BaseStruct);
+	/* Start count on TIM4 */
+    TIM_Cmd(TIM1, ENABLE);
+}
+
+void TM_PWM_Init(void) {
+	TIM_OCInitTypeDef TIM_OCStruct;
+
+	/* Common settings */
+
+	/* PWM mode 2 = Clear on compare match */
+	/* PWM mode 1 = Set on compare match */
+	TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM2;
+	TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
+
+/*
+	To get proper duty cycle, you have simple equation
+
+	pulse_length = ((TIM_Period + 1) * DutyCycle) / 100 - 1
+
+	where DutyCycle is in percent, between 0 and 100%
+
+	25% duty cycle: 	pulse_length = ((8399 + 1) * 25) / 100 - 1 = 2099
+	50% duty cycle: 	pulse_length = ((8399 + 1) * 50) / 100 - 1 = 4199
+	75% duty cycle: 	pulse_length = ((8399 + 1) * 75) / 100 - 1 = 6299
+	100% duty cycle:	pulse_length = ((8399 + 1) * 100) / 100 - 1 = 8399
+
+	Remember: if pulse_length is larger than TIM_Period, you will have output HIGH all the time
+*/
+	TIM_OCStruct.TIM_Pulse = 2099; /* 25% duty cycle */
+	TIM_OC1Init(TIM1, &TIM_OCStruct);
+	TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+	TIM_OCStruct.TIM_Pulse = 4199; /* 50% duty cycle */
+	TIM_OC2Init(TIM1, &TIM_OCStruct);
+	TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+	TIM_OCStruct.TIM_Pulse = 6299; /* 75% duty cycle */
+	TIM_OC3Init(TIM1, &TIM_OCStruct);
+	TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+	TIM_OCStruct.TIM_Pulse = 8399; /* 100% duty cycle */
+	TIM_OC4Init(TIM1, &TIM_OCStruct);
+	TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
+}
+
+int main(void) {
+
+  Configure_PC4();
+
+  init_USART1(9600); // initialize USART1 @ 9600 baud
+
+  //init_I2C1(); //
+
+  TIM_INT_Init();
+
+  /* Init leds */
+  TM_LEDS_Init();
+  /* Init timer */
+  TM_TIMER_Init();
+  /* Init PWM */
+  TM_PWM_Init();
+
+
+  while (1){
+    /*
+     * You can do whatever you want in here
+     */
+	  //__WFI();
+  }
+}
+
+// this is the interrupt request handler (IRQ) for ALL USART1 interrupts
+void USART1_IRQHandler(void){
+
+	// check if the USART1 receive interrupt flag was set
+	if( USART_GetITStatus(USART1, USART_IT_RXNE) ){
+
+		static uint8_t cnt = 0; // this counter is used to determine the string length
+		char t = USART1->DR; // the character from the USART1 data register is saved in t
+
+		/* check if the received character is not the LF character (used to determine end of string)
+		 * or the if the maximum string length has been been reached
+		 */
+		if( (t != '\n') && (cnt < MAX_STRLEN) ){
+			received_string[cnt] = t;
+			cnt++;
+		}
+		else{ // otherwise reset the character counter and print the received string
+			cnt = 0;
+		}
+	}
+}
+
+void EXTI4_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+    if (EXTI_GetITStatus(EXTI_Line4) != RESET) {
+        /* Do your stuff when PD0 is changed */
+    	uint32_t time = 0;
+
+    	while(!(GPIOC->IDR & GPIO_Pin_4) && (time < BUTTON_LIMIT)){
+    		time = time + 1;
+    	}
+
+    	// if state is equal to 1
+    	if(state == 1){
+    		if(time < BUTTON_LIMIT){
+    			USART_puts(USART1, "hello");
+    			state = 2;
+    		}
+    		else{
+    			USART_puts(USART1, "quit");
+    			state = 1;
+    		}
+    	}
+    	else{
+    		if(time < BUTTON_LIMIT){
+    			USART_puts(USART1, "data");
+    			state = 2;
+    		}
+    		else{
+    			USART_puts(USART1, "quit");
+    			state = 1;
+    		}
+    	}
+
+    	while(!(GPIOC->IDR & GPIO_Pin_4)){}
+
+        /* Clear interrupt flag */
+        EXTI_ClearITPendingBit(EXTI_Line4);
+    }
+}
+
